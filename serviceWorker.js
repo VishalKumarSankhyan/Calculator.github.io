57045d59d168c0bee011de4c2cd09dcad6533e66
GF/2020/4552   Vishal Kumar Sankhyan <71447579+VishalKumarSankhyan@users.noreply.github.com>
2021-07-31 17:17:37 +0530

    Add files via upload

---
+++ serviceWorker.js
@@ -0,0 +1,26 @@
+var cacheName = 'Calculator';
+var cachefiles = [
+    '/',/*url*/
+    'index.html',
+    'calculator.css',
+    'calculator.js',
+    'app.js',
+    'calculator_icon.ico',
+    'calculator.png',
+];
+
+self.addEventListener("install", installEvent => {
+    installEvent.waitUntill(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(cachefiles)
+        })
+    )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res=>{
+            return res || fetch(fetchEvent.request)
+        })
+    )
+});
